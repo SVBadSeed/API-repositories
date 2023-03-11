f327973c53c254b7074eb4b2201e665fe92310cc
Masthead <alexeev.tolik2015@yandex.ru>
2023-03-11 16:51:02 +0300

    gitAPI

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     devtool: "source-map",
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         clean: true,
         filename: 'bundle.js'
     },
